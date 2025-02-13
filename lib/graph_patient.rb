f083b7911c39d5b455d538ff47c2311b1cc5bde0
Misha Gorodnitzky <misaka@dmfg.io>
2025-02-13 15:21:39 +0000

    WIP

--- lib/graph_patient.rb
+++ lib/graph_patient.rb
@@ -4,23 +4,25 @@ class GraphPatient
   def initialize(
     *patients,
     parents: [],
-    include_consents: true,
-    include_class_imports: false,
-    highlight_patients: [],
-    highlight_parents: []
+    show_consents: true,
+    show_class_imports: true,
+    show_cohort_imports: true,
+    focus_patients: [],
+    focus_parents: []
   )
     @patients = patients.map { it.is_a?(Patient) ? it : Patient.find(it) }
     @parents = parents.map { it.is_a?(Parent) ? it : Parent.find(it) }
-    @include_consents = include_consents
-    @include_class_imports = include_class_imports
-    @highlight_patients =
+    @show_consents = show_consents
+    @show_class_imports = show_class_imports
+    @show_cohort_imports = show_cohort_imports
+    @focus_patients =
       @patients +
-        Array(highlight_patients).map! do |it|
+        Array(focus_patients).map! do |it|
           it.is_a?(Patient) ? it : Patient.find(it)
         end
-    @highlight_parents =
+    @focus_parents =
       @parents +
-        Array(highlight_parents).map! do |it|
+        Array(focus_parents).map! do |it|
           it.is_a?(Parent) ? it : Parent.find(it)
         end
 
@@ -39,8 +41,9 @@ class GraphPatient
       "  classDef parent fill:#faa0a0",
       "  classDef consent fill:#fffaa0",
       "  classDef class_import fill:#7fd7df",
-      "  classDef patient_highlighted fill:#c2e598,stroke:#000,stroke-width:3px",
-      "  classDef parent_highlighted fill:#faa0a0,stroke:#000,stroke-width:3px"
+      "  classDef cohort_import fill:#a2d2ff",
+      "  classDef patient_focused fill:#c2e598,stroke:#000,stroke-width:3px",
+      "  classDef parent_focused fill:#faa0a0,stroke:#000,stroke-width:3px"
     ]
   end
 
@@ -50,14 +53,15 @@ class GraphPatient
     @visited_patients << patient
 
     node = patient_node(patient)
-    parents = patient.parents.includes(:consents, :class_imports)
+    parents =
+      patient.parents.includes(:consents, :class_imports, :cohort_imports)
     parent_connections =
       parents.flat_map do
         ["  #{node} --> #{parent_node(it)}"] + parent_graph(it)
       end
 
     ["  #{node}"] + parent_connections + consent_connections(patient) +
-      class_imports_connections(patient)
+      class_imports_connections(patient) + cohort_imports_connections(patient)
   end
 
   def parent_graph(parent)
@@ -65,11 +69,13 @@ class GraphPatient
 
     @visited_parents << parent
 
-    patients = parent.patients.includes(:parents, :class_imports)
+    patients =
+      parent.patients.includes(:parents, :class_imports, :cohort_imports)
     patient_connections = patients.flat_map { patient_graph(it) }
 
     ["  #{parent_node(parent)}"] + patient_connections +
-      consent_connections(parent) + class_imports_connections(parent)
+      consent_connections(parent) + class_imports_connections(parent) +
+      cohort_imports_connections(parent)
   end
 
   def patient_node(patient)
@@ -88,8 +94,14 @@ class GraphPatient
     "class_import-#{class_import.id}:::#{class_for_class_import(class_import)}"
   end
 
+  def node_text(obj)
+    klass = obj.class.name.underscore
+    diagram_class_name = class_text_for_obj(obj)
+    "#{klass}-#{obj.id}:::#{diagram_class_name}"
+  end
+
   def consent_connections(obj)
-    return [] unless @include_consents
+    return [] unless @show_consents
 
     case obj
     when Patient
@@ -102,7 +114,7 @@ class GraphPatient
   end
 
   def class_imports_connections(obj)
-    return [] unless @include_class_imports
+    return [] unless @show_class_imports
 
     obj_node =
       case obj
@@ -114,12 +126,25 @@ class GraphPatient
     obj.class_imports.map { "  #{class_import_node(it)} --> #{obj_node}" }
   end
 
+  def cohort_imports_connections(obj)
+    return [] unless @show_cohort_imports
+
+    obj_node =
+      case obj
+      when Patient
+        patient_node(obj)
+      when Parent
+        parent_node(obj)
+      end
+    obj.cohort_imports.map { "  #{node_text(it)} --> #{obj_node}" }
+  end
+
   def class_for_patient(patient)
-    @highlight_patients.include?(patient) ? "patient_highlighted" : "patient"
+    @focus_patients.include?(patient) ? "patient_focused" : "patient"
   end
 
   def class_for_parent(parent)
-    @highlight_parents.include?(parent) ? "parent_highlighted" : "parent"
+    @focus_parents.include?(parent) ? "parent_focused" : "parent"
   end
 
   def class_for_consent(_consent)
@@ -128,5 +153,9 @@ class GraphPatient
 
   def class_for_class_import(_class_import)
     "class_import"
+  end
+
+  def class_text_for_obj(obj)
+    obj.class.name.underscore
   end
 end
--- spec/lib/graph_patient_spec.rb
+++ spec/lib/graph_patient_spec.rb
@@ -1,43 +1,58 @@
 describe GraphPatient do
-  it "should return a flowchart" do
-    programme = create(:programme, :hpv)
-    organisation = create(:organisation, programmes: [programme])
-    session = create(:session, organisation:, programmes: [programme])
-    class_import = create(:class_import, session:)
-    parent = create(:parent, class_imports: [class_import])
-    patient =
-      create(
-        :patient,
-        parents: [parent],
-        session:,
-        organisation:,
-        programme:,
-        class_imports: [class_import]
-      )
-    consent =
-      create(:consent, :given, patient:, parent:, organisation:, programme:)
-    parent = patient.parents.first
+  subject(:graph) { described_class.new(patient).call }
 
-    graph = described_class.new(patient, include_class_imports: true).call
+  let!(:programme) { create(:programme, :hpv) }
+  let!(:organisation) { create(:organisation, programmes: [programme]) }
+  let!(:session) { create(:session, organisation:, programmes: [programme]) }
+  let!(:class_import) { create(:class_import, session:) }
+  let!(:cohort_import) { create(:cohort_import, organisation:) }
+  let!(:parent) do
+    create(
+      :parent,
+      class_imports: [class_import],
+      cohort_imports: [cohort_import]
+    )
+  end
+  let!(:patient) do
+    create(
+      :patient,
+      parents: [parent],
+      session:,
+      organisation:,
+      programme:,
+      class_imports: [class_import],
+      cohort_imports: [cohort_import]
+    )
+  end
+  let!(:consent) do
+    create(:consent, :given, patient:, parent:, organisation:, programme:)
+  end
 
-    expected = [
+  it { should start_with "flowchart TB" }
+
+  it "generates the graph" do
+    puts graph
+    expect(graph).to contain_exactly(
       "flowchart TB",
       "  classDef patient fill:#c2e598",
       "  classDef parent fill:#faa0a0",
       "  classDef consent fill:#fffaa0",
       "  classDef class_import fill:#7fd7df",
-      "  classDef patient_highlighted fill:#c2e598,stroke:#000,stroke-width:3px",
-      "  classDef parent_highlighted fill:#faa0a0,stroke:#000,stroke-width:3px",
-      "  patient-#{patient.id}:::patient_highlighted",
-      "  patient-#{patient.id}:::patient_highlighted --> parent-#{parent.id}:::parent",
+      "  classDef patient_focused fill:#c2e598,stroke:#000,stroke-width:3px",
+      "  classDef parent_focused fill:#faa0a0,stroke:#000,stroke-width:3px",
+      "  classDef cohort_import fill:#a2d2ff",
+      "  patient-#{patient.id}:::patient_focused",
       "  parent-#{parent.id}:::parent",
-      "  consent-#{consent.id}:::consent --> parent-#{parent.id}:::parent",
-      "  class_import-#{class_import.id}:::class_import --> parent-#{parent.id}:::parent",
-      "  patient-#{patient.id}:::patient_highlighted --> consent-#{consent.id}:::consent",
-      "  class_import-#{class_import.id}:::class_import --> patient-#{patient.id}:::patient_highlighted"
-    ]
-    graph.flatten.each_with_index do |line, index|
-      expect(line).to eq(expected[index])
-    end
+      # "  consent-#{consent.id}:::consent",
+      # "  class_import-#{class_import.id}:::class_import",
+      # "  cohort_import-#{cohort_import.id}:::cohort_import",
+      "  patient-#{patient.id} --> parent-#{parent.id}",
+      "  consent-#{consent.id} --> parent-#{parent.id}",
+      "  class_import-#{class_import.id} --> parent-#{parent.id}",
+      "  cohort_import-#{cohort_import.id} --> parent-#{parent.id}",
+      "  patient-#{patient.id} --> consent-#{consent.id}",
+      "  class_import-#{class_import.id} --> patient-#{patient.id}",
+      "  cohort_import-#{cohort_import.id} --> patient-#{patient.id}"
+    )
   end
 end
