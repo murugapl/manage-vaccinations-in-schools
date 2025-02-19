481593152bdf5ad698ab932fb1e5e052e0a88352
Misha Gorodnitzky <misaka@dmfg.io>
2025-02-19 23:31:53 +0000

    WIP

--- lib/graph_patient.rb
+++ lib/graph_patient.rb
@@ -4,9 +4,6 @@ class GraphPatient
   def initialize(
     *patient_ids,
     parents: [],
-    show_consents: true,
-    show_class_imports: true,
-    show_cohort_imports: true,
     focus_patients: [],
     focus_parents: [],
     node_order: %i[class_import cohort_import patient consent parent]
@@ -72,17 +69,13 @@ class GraphPatient
     @nodes.to_a.map { "  #{node_with_class(it)}" }
   end
 
-  def order_nodes(*nodes)
-    nodes.sort_by { @node_order.index(it.class.name.underscore.to_sym) }
-  end
-
   def render_edges
     @edges.map { |from, to| "  #{node_name(from)} --> #{node_name(to)}" }
   end
 
   def inspect(obj)
     associations_list = @inspection_list[obj.class.name.underscore.to_sym]
-    return unless associations_list.present?
+    return if associations_list.blank?
 
     return if @inspected.include?(obj)
     @inspected << obj
@@ -115,6 +108,10 @@ class GraphPatient
 
   def associated_patients_objects(base)
     base.patients.includes(:parents, :class_imports, :cohort_imports)
+  end
+
+  def order_nodes(*nodes)
+    nodes.sort_by { @node_order.index(it.class.name.underscore.to_sym) }
   end
 
   def node_name(obj)
